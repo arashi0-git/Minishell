2be52fb789ebe903f6bf0821a3ba9f92574426f7
unknown <sfa00309>
2025-01-30 14:38:00 +0900

    add

---
+++ srcs/builtin/process_cd.c
@@ -0,0 +1,15 @@
+
+#include "../include/minishell.h"
+
+int exec_cd(char **args){
+    char *path = args[1];
+    char oldpwd[PATH_MAX];
+    char newpwd[PATH_MAX];
+    char *target;
+
+    if(getcwd(oldpwd, sizeof(oldpwd))==NULL){
+        perror("cd: getcwd");
+        return(1);
+    }
+}
+
