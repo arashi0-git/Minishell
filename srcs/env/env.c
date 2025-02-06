5390bf5546473d31525218727c6a8acce7e56dd7
harumoren2507 <kuroshiro0725@gmail.com>
2025-02-06 22:26:39 +0900

    fix: init_envにバグがあったので修正＆テスト作成

--- srcs/env/env.c
+++ srcs/env/env.c
@@ -6,7 +6,7 @@
 /*   By: aryamamo <aryamamo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/01 11:05:25 by aryamamo          #+#    #+#             */
-/*   Updated: 2025/02/01 11:17:39 by aryamamo         ###   ########.fr       */
+/*   Updated: 2025/02/06 22:25:31 by retoriya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -78,23 +78,57 @@ int	set_env(t_env **env, const char *name, const char *value)
 
 t_env	*init_env(char **env)
 {
-	t_env *env = NULL;
-	t_env *new_env;
-	int i = 0;
-	char *equal_sign;
+	t_env	*env_list;
+	t_env	*new_env;
+	int		i;
+	char	*equal_sign;
+
+	env_list = NULL;
+	i = 0;
 	while (env[i])
 	{
 		equal_sign = ft_strchr(env[i], '=');
-		if (!equal_sign)
-			i++;
-		new_env = malloc(sizeof(t_env));
-		if (!new_env)
-			return (NULL);
-		new_env->key = ft_strndup(env[i], (equal_sign - env[i]));
-		new_env->value = ft_strdup(equal_sign + 1);
-		new_env->next = env;
-		env = new_env;
+		if (equal_sign)
+		{
+			new_env = malloc(sizeof(t_env));
+			if (!new_env)
+				return (NULL);
+			new_env->key = ft_strndup(env[i], equal_sign - env[i]);
+			new_env->value = ft_strdup(equal_sign + 1);
+			new_env->next = env_list;
+			env_list = new_env;
+		}
 		i++;
 	}
-	return (env);
+	return (env_list);
 }
+
+/*
+ //libft をmake & env/でcc -Wall -Wextra -Werror env.c -I../../include -L../../libft -lft でテスト可能
+int	main(int argc, char *argv[], char **envp)
+{
+	t_env	*env_list;
+	t_env	*current;
+
+	(void)argc;
+	(void)argv;
+	env_list = init_env(envp);
+	if (!envp)
+	{
+		printf("初期化失敗\n");
+		return (1);
+	}
+	current = env_list;
+	while (current)
+	{
+		printf("key: %s, value: %s\n", current->key, current->value);
+		current = current->next;
+	}
+	free_env(env_list);
+	return (0);
+}
+
+ init_envがすべきこと：
+プログラム起動時に、親プロセス（通常のシェル）から渡される環境変数の配列（char **envp）を受け取る
+その環境変数を、minishellで使いやすい形（連結リスト）に変換して保存する
+*/
