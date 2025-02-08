52edd9dfaac0afb8100e8b7009a37bd5d6909051
harumoren2507 <kuroshiro0725@gmail.com>
2025-02-08 21:30:31 +0900

    add:exportの枠組み作成

--- srcs/builtin/process_export.c
+++ srcs/builtin/process_export.c
@@ -1,6 +1,45 @@
+static bool is_valid_identifier(const char *str)
+{
+    char *equal_pos;
+    size_t  key_length;
+    size_t  i;
+    
+    equal_pos = NULL;
+    i = 0;
+    if (!str || !*str)
+        return (false);
+    if (!ft_is_alpha(*str) || *str != '_')
+        return (false);
+    equal_pos = ft_strchr(str, '=');
+    if (equal_pos)
+        key_length = equal_pos - s;
+    else 
+        key_length = ft_strlen(str);
+    i = 1;
+    while (i < key_length)
+    {
+        if (!ft_is_alnum(str[i]) && str[i] != '_')
+            return (false);
+        i++;
+    }
+    return (true);
+}
 
+int	exec_export(char **args, t_shell *shell)
+{
+	int i;
 
-
+    if (!args[1])
+		//return (print_sorted_env(shell->env));
+    i = 1;
+    while (args[i])
+    {
+        if (is_valid_identifier(args[i]))
+            //update_env_variable(shell->env, args[i]);
+        i++;
+    }
+    return (0);
+}
 /*
    export [-fn] [name[=word]] ...
        export -p
