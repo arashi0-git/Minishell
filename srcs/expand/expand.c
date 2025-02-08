c22fd3e8e023e7f51838f1f9e30fa4c360978299
Arashi Yamamoto <aryamamo@c6r3s1.42tokyo.jp>
2025-02-08 18:03:34 +0900

    expand process

--- srcs/expand/expand.c
+++ srcs/expand/expand.c
@@ -6,8 +6,35 @@
 /*   By: aryamamo <aryamamo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/08 17:06:24 by aryamamo          #+#    #+#             */
-/*   Updated: 2025/02/08 17:11:44 by aryamamo         ###   ########.fr       */
+/*   Updated: 2025/02/08 17:48:38 by aryamamo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
+
+void	expand_cmd(t_cmd *cmd, t_shell *shell)
+{
+	int		i;
+	char	*expanded;
+
+	if (cmd->command)
+	{
+		expanded = expand(cmd->command, shell);
+		if (expanded)
+		{
+			free(cmd->command);
+			cmd->command = expanded;
+		}
+	}
+	i = 0;
+	while (cmd->args && cmd->args[i])
+	{
+		expanded = expand(cmd->args[i], shell);
+		if (expanded)
+		{
+			free(cmd->args[i]);
+			cmd->args[i] = expanded;
+		}
+		i++;
+	}
+}
