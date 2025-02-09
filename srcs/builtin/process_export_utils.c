cfe8afb60e0b26b4408ce3e8e0ca8aa6f9867a5d
harumoren2507 <kuroshiro0725@gmail.com>
2025-02-09 19:30:21 +0900

    add:export関数の初期実装を作成:leak対応などの細部はまだ未対応

---
+++ srcs/builtin/process_export_utils.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   process_export_utils.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: retoriya <retoriya@student.42tokyo.jp      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/09 15:23:55 by retoriya          #+#    #+#             */
+/*   Updated: 2025/02/09 17:56:11 by retoriya         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/minishell.h"
+
+static void	print_export_format(const char *env_str)
+{
+	char	*equal_pos;
+
+	ft_putstr_fd("declare -x ", STDOUT_FILENO);
+	equal_pos = ft_strchr(env_str, '=');
+	if (equal_pos)
+	{
+		write(STDOUT_FILENO, env_str, equal_pos - env_str);
+		ft_putstr_fd("=\"", STDOUT_FILENO);
+		ft_putstr_fd(equal_pos + 1, STDOUT_FILENO);
+		ft_putstr_fd("\"", STDOUT_FILENO);
+	}
+	else
+		ft_putstr_fd(env_str, STDOUT_FILENO);
+	ft_putstr_fd("\n", STDOUT_FILENO);
+}
+
+int	print_sorted_env(t_env *env)
+{
+	char	**sorted_env;
+	int		i;
+
+	sorted_env = sort_env(env);
+	if (!sorted_env)
+		return (1);
+	i = 0;
+	while (sorted_env[i])
+	{
+		print_export_format(sorted_env[i]);
+		i++;
+	}
+	// free_array(sorted_env);
+	return (0);
+}
