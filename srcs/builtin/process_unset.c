c1fd776ec20e96557177a6f787483bb66b7606b8
harumoren2507 <kuroshiro0725@gmail.com>
2025-02-11 16:59:26 +0900

    unsetのkey＆要素削除機能を実装

--- srcs/builtin/process_unset.c
+++ srcs/builtin/process_unset.c
@@ -6,13 +6,48 @@
 /*   By: retoriya <retoriya@student.42tokyo.jp      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/11 13:03:31 by retoriya          #+#    #+#             */
-/*   Updated: 2025/02/11 13:11:34 by retoriya         ###   ########.fr       */
+/*   Updated: 2025/02/11 16:57:07 by retoriya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-//たたき台
+static	void	free_env_node(t_env* node)
+{
+	if (!node)
+		return ;
+	free(node->key);
+	free(node->value);
+	free(node);
+}
+
+void	remove_env_unset(t_shell *shell, char *arg)
+{
+	t_env *current;
+	t_env *prev;
+
+	if (ft_strcmp(shell->env->key, arg) == 0)
+	{
+		current = shell->env;
+		shell->env = shell->env->next;
+		free_env_node(current);
+		return ;
+	}
+	prev = shell->env;
+	current = shell->env->next;
+	while (current && ft_strcmp(current->key, arg) != 0)
+	{
+		prev = current;
+		current = current->next;
+	}
+	if (current)
+	{
+		prev->next = current->next;
+		free_env_node(current);
+		return ;
+	}
+}
+
 int    cmd_unset(char **args, t_shell *shell)
 {
     int        i;
